67184c2e3a117be5e4219c71948c9c368e2d510d
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-24 21:49:41 -0400

    testing

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,44 @@ import * as typing from "./toolkit/typing.js";
 
 const HtmlService = services.HtmlService;
 const Element = HtmlService.Element;
+const Document = HtmlService.Document;
 
-let h1 = typing.nullbut(HTMLHeadingElement);
-
-if (!h1) {
-    h1 = Element.new("h1", document.body);
-    Element.setId(h1, "texto");
+const cssCode = /* css */ `
+div.shape {
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    background-color: red;
 }
 
-Element.setText(Element.getAllByQuery("h1#texto"), "Hello, World!");
+@keyframes rotate {
+    100% {
+        transform: translate(-50%, -50%) rotate(360deg);
+    }
+}
+`
+Document.loadCSS("main", cssCode)
+
+const div = Element.new("div", document.body)
+Element.setClass( div, [ "shape" ] )
+Element.setCSS(div, {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+})
+
+const inner = Element.new("div", div);
+
+Element.setCSS(inner, {
+    position: "absolute",
+    width: "30px",
+    height: "30px",
+    backgroundColor: "white",
+
+    // centralizado dentro do vermelho
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+});
+
+Element.animate(div, "rotate 2s cubic-bezier(.85,.12,.18,.87) infinite")
