14aff3547642d9d2eb28448385b0218e956b4e7e
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-24 21:17:17 -0400

    testes em index

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,14 @@
-import * as types from "./toolkit/typing.js";
+import * as services from "./toolkit/services.js";
+import * as typing from "./toolkit/typing.js";
 
+const HtmlService = services.HtmlService;
+const Element = HtmlService.Element;
+
+let h1 = typing.nullbut(HTMLHeadingElement);
+
+if (!h1) {
+    h1 = Element.new("h1", document.body);
+    Element.setId(h1, "texto");
+}
+
+Element.setText(Element.getAllByQuery("h1#texto"), "Hello, World!");
