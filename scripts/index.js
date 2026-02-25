dc553f0db8726311ac4771899f973c74838550dc
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-24 20:11:21 -0400

    (script.toolkit.index) reset

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,2 @@
 import * as types from "./toolkit/types.js";
 
-const Pessoa = types.object({
-    nome: types.string,
-    idade: types.number
-});
-
-const novaPessoa = types.generator(types.createDefault(Pessoa, {"string": "desconhecido", "number": 18}));
-
-let Pedro = novaPessoa();
-
-console.log(Pessoa);
-console.log(Pedro);
