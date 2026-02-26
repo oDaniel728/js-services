8fe85fe4e1358253b7c3ad173cc31d74f1b8fe79
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-25 21:08:09 -0400

    testing

--- scripts/index.js
+++ scripts/index.js
@@ -5,42 +5,20 @@ const HtmlService = services.HtmlService;
 const Element = HtmlService.Element;
 const Document = HtmlService.Document;
 
-const cssCode = /* css */ `
-div.shape {
-    position: absolute;
-    width: 200px;
-    height: 200px;
-    background-color: red;
+const htmlCode = /* html */`<ul id="console"></ul>`;
+Document.loadHTML(htmlCode, 'div', document.body);
+
+const console = HtmlService.Console.new("#console", "li");
+// styles em "/styles/index.css"
+
+console.log("Hello, World!");
+
+for (let i = 0; i < 10; i++) {
+    const n = i + 1;
+    if (n % 5 == 0)
+        console.error(n);
+    else if (n % 3 == 0)
+        console.log(n);
+    else
+        console.warn(n);
 }
-
-@keyframes rotate {
-    100% {
-        transform: translate(-50%, -50%) rotate(360deg);
-    }
-}
-`
-Document.loadCSS("main", cssCode)
-
-const div = Element.new("div", document.body)
-Element.setClass( div, [ "shape" ] )
-Element.setCSS(div, {
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-})
-
-const inner = Element.new("div", div);
-
-Element.setCSS(inner, {
-    position: "absolute",
-    width: "30px",
-    height: "30px",
-    backgroundColor: "white",
-
-    // centralizado dentro do vermelho
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)"
-});
-
-Element.animate(div, "rotate 2s cubic-bezier(.85,.12,.18,.87) infinite")
