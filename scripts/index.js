78e9bcfee8e105f69e49200368ba0153e6c684b0
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-26 11:09:24 -0400

    + (scripts.toolkit.services.LocalStorageService.LocalStorage) hook(), evaluateKey(key, cb)

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,28 @@
 import * as services from "./toolkit/services.js";
+
+// Carrega serviços
 const HtmlService = services.HtmlService;
 const LocalStorageService = services.LocalStorageService;
+const Element = HtmlService.Element;
+const DocProvider = HtmlService.Document;
 
+// Carrega elementos
+const textAreaText = Element.getByQueryForce("textarea#text");
 
+// Cria a sessão
 const Session = {
     notepadContent: ""
 };
 const SessionStorage = LocalStorageService.new("SessionStorage", Session);
+SessionStorage.hook(); // Carrega os eventos ( load e unload )
+// - Quando entrar no site: carrega o Storage;
+// - Quando sair do site: salva o Storage.
 
-HtmlService.Element.getByQueryForce("textarea#text").addEventListener("input", /** @param { InputEvent & { target: HTMLTextAreaElement } } e */
+// Carrega o texto no textAreaText
+window.addEventListener("load", () => { textAreaText.value = Session.notepadContent })
+
+// Quando digitar em textAreaText: Salva no session
+textAreaText.addEventListener("input", /** @param { InputEvent & { target: HTMLTextAreaElement } } e */
 function(e) {
     Session.notepadContent = e.target.value
 });
-
-window.addEventListener("load", (e) => {
-    SessionStorage.load();
-    HtmlService.Element.getByQueryForce("textarea#text").value = Session.notepadContent;
-})
-window.addEventListener("beforeunload", (e) => {
-    SessionStorage.save();
-})
