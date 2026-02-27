eb0854f3cc6fc9aa2c7ba4051469459ea8d024e6
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-27 19:43:13 -0400

    [26.02.27] testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,53 @@
 // @ts-check
-import * as typing from "./toolkit/typing.js";
 import * as services from "./toolkit/services.js";
-const FactoryService = services.GetService("FactoryService");
 
-const tPessoa = FactoryService.Struct.new(
-    ["nome", "idade"],
+const EventService = services.GetService("EventService");
+const LocalStorageService = services.GetService("LocalStorageService");
+const Element = services.GetService("HtmlService").Element;
+
+const Session=
+{
+    running: false,
+    time: 0,
+
+    stop() 
     {
-        nome: typing.string,
-        idade: typing.number,
-
-        dizerOla() {
-            console.log(`Olá, eu sou ${this.nome}`);
-        }
-    }
-)
-const tContaBancaria = FactoryService.Struct.new(
-    ["saldo"],
+        this.running = false;
+    },
+    step()
     {
-        saldo: typing.optional(typing.number),
-
-        verSaldo() {
-            console.log(`Eu tenho ${this.saldo ?? 0} reais!`);
-        }
+        this.time += 1;
     }
-)
+};
+const SessionStorage = LocalStorageService.new("session-storage", Session);
+SessionStorage.hook();
 
-const tPessoaFisica = FactoryService.Struct.inherits(tContaBancaria, tPessoa);
-const PessoaFisica = FactoryService.Factory.new(tPessoaFisica);
+const onStart = EventService.Event.new();
+const onLoop  = EventService.Event.new();
+const onExit  = EventService.Event.new();
 
-const pedro = PessoaFisica.new("Pedro", 16)
-pedro.saldo = 100;
-pedro.dizerOla();
-pedro.verSaldo();
+onStart.connect(() => {
+    console.log("Started!");
+});
+
+const text1 = Element.getByQueryForce("p#text1");
+onLoop.connect(() => {
+    Session.stop();
+});
+onExit.connect(() => {
+    console.log("Ended!");
+});
+function main()
+{
+    onStart.emit();
+
+    Session.running = true;
+    while(Session.running)
+    {
+        onLoop.emit();
+    }
+
+    onExit.emit();
+}
+
+main();
