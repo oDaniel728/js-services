5ed2e33af148fa22ca598ba5443fc62497d475be
oDaniel728 <danielmenezes0822@gmail.com>
2026-02-28 22:33:47 -0400

    [26.02.28] testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,29 @@
 // @ts-check
 import * as services from "./toolkit/services.js";
+import * as typing from "./toolkit/typing.js";
+const FactoryService = services.getService("FactoryService");
 
-const EventService = services.GetService("EventService");
-const LocalStorageService = services.GetService("LocalStorageService");
-const Element = services.GetService("HtmlService").Element;
-
-const Session=
-{
-    running: false,
-    time: 0,
-
-    stop() 
-    {
-        this.running = false;
-    },
-    step()
-    {
-        this.time += 1;
+const tCor = typing.literal("verde", "amarelo", "vermelho")
+const mapaDeCor = typing.as(typing.dict(tCor, tCor), {
+    "verde": "amarelo",
+    "amarelo": "vermelho",
+    "vermelho": "verde"
+})
+const tSinal = typing.object({
+    cor: tCor,
+    
+    /**
+     * @param { typeof tCor? } cor 
+     */
+    mudarCor(cor = null) {
+        this.cor = cor ?? mapaDeCor[this.cor];
     }
-};
-const SessionStorage = LocalStorageService.new("session-storage", Session);
-SessionStorage.hook();
+})
 
-const onStart = EventService.Event.new();
-const onLoop  = EventService.Event.new();
-const onExit  = EventService.Event.new();
+const sSinal = FactoryService.Struct.new(["cor"], tSinal);
+const Sinal = FactoryService.Factory.new(sSinal);
 
-onStart.connect(() => {
-    console.log("Started!");
-});
-
-const text1 = Element.getByQueryForce("p#text1");
-onLoop.connect(() => {
-    Session.stop();
-});
-onExit.connect(() => {
-    console.log("Ended!");
-});
-function main()
-{
-    onStart.emit();
-
-    Session.running = true;
-    while(Session.running)
-    {
-        onLoop.emit();
-    }
-
-    onExit.emit();
-}
-
-main();
+const sinal = Sinal.new("verde");
+sinal.mudarCor();
+sinal.mudarCor();
+sinal.mudarCor();
